1e3f285a3193e739d641240233210f16a24e173b
davidcorredor13 <64997596+davidcorredor13@users.noreply.github.com>
2021-10-13 10:55:09 -0500

    Update usuarios.sql
    David agrego todos los insert del data

--- persistencia/src/test/resources/usuarios.sql
+++ persistencia/src/test/resources/usuarios.sql
@@ -1,10 +1,40 @@
+
 insert into ciudad values (1, "Armenia");
 insert into ciudad values (2, "Pereira");
 
-insert into usuario values ("123", "2000/10/09" , "MASCULINO", "Carlos", "carlos@email.com", 1);
-insert into usuario values ("124", "2000/10/09" , "FEMENINO", "Maria", "maria@email.com", 2);
-insert into usuario values ("125", "2000/10/09" , "MASCULINO", "Luis", "Luis@email.com", 2);
+insert into usuario values ("123", "Pepe" , "pepe@email.com", "13434", "pepe", 1);
+insert into usuario values ("124", "Maria" , "maria@email.com", "43433", "maria", 2);
+insert into usuario values ("125", "Juan" , "juan@email.com", "3333", "juan", 1);
 
-insert into usuario_num_telefonos values ("123", "718718", "casa");
-insert into usuario_num_telefonos values ("123", "2232", "trabajo");
+insert into usuario_telefonos values ("123", "718718");
+insert into usuario_telefonos values ("123", "2232");
 
+
+insert into producto values (1, "Telefono Samsung s21 como nuevo", 0 , "2021/10/30", "Samsung S21", "Samsung S21 x", 4_000_000,3,1,"123");
+insert into producto values (2, "Nintendo Switch nueva sellada", 0 , "2021/10/30", "Nintendo Switch", "Nintendo Switch OLED", 2000000,2,2,"124");
+insert into producto values (3, "Tennis Adidas Originales", 0.05 , "2021/10/30", "Tennis Adidas", "Tennis Adidas originales", 230_000,3,2,"124");
+
+insert into producto_categorias values (1, 0);
+insert into producto_categorias values (2, 0);
+insert into producto_categorias values (3, 1);
+insert into producto_categorias values (3, 2);
+
+insert into producto_imagenes values (1, "ruta1/img1.jpg");
+insert into producto_imagenes values (1, "ruta1/img1.jpg");
+insert into producto_imagenes values (2, "ruta2/img.jpg");
+insert into producto_imagenes values (3, "ruta3/img2.jpg");
+insert into producto_imagenes values (3, "ruta3/img1.jpg");
+
+insert into compra values (1, "2021/10/05 20:12:05", "PSE", "125");
+insert into compra values (2, "2021/10/05 12:10:09", "tarjeta crédito VISA", "123");
+
+insert into detalle_compra values(1, 2_000_000, 1, 1, 2);
+insert into detalle_compra values(2, 225_000, 1, 1, 3);
+insert into detalle_compra values(3, 4_000_000, 1, 2, 1);
+
+insert into comentario values (1, 3, "2021/10/06 07:12:32", "Producto regular, no me gustó tanto", "", 3, "125");
+insert into comentario values (2, 5, "2021/10/07 13:29:48", "Excelente celular, xd", "", , "123");
+
+insert into usuario_productos_favoritos values ("123", 2);
+insert into usuario_productos_favoritos values ("123", 3);
+insert into usuario_productos_favoritos values ("125", 2);
