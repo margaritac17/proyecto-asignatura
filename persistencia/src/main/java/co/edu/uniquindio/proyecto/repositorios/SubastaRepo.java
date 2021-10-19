44fc41e385fa30ef6105bd4b862c6da2dc5c3f41
Diego Alexander <diegoa.ipialeso@uqvirtual.edu.co>
2021-10-18 23:36:34 -0500

    Se crea repositorio y test detallesubasta

--- persistencia/src/main/java/co/edu/uniquindio/proyecto/repositorios/SubastaRepo.java
+++ persistencia/src/main/java/co/edu/uniquindio/proyecto/repositorios/SubastaRepo.java
@@ -1,10 +1,6 @@
 package co.edu.uniquindio.proyecto.repositorios;
 
-<<<<<<< HEAD
 
-
-=======
->>>>>>> 48a1ca8df3f0e2964adb8756c6bb238e032a8ffb
 import co.edu.uniquindio.proyecto.entidades.Subasta;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
